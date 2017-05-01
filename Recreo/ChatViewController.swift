bbf569cbb0925c2102b86fd283b5fb1165dcd67f
Angie Lal <angielal@gmail.com>
2017-04-30 23:28:02 -0700

    Add gallery collection and chat table view controllers.

---
+++ Recreo/ChatViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ChatViewController.swift
+//  Recreo
+//
+//  Created by Angie Lal on 4/30/17.
+//  Copyright © 2017 Intuit. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatTableViewCell
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recreo/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Recreo
+//
+//  Created by Angie Lal on 4/30/17.
+//  Copyright © 2017 Intuit. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+}
