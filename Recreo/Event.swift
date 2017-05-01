e453fe6e21404f7865c4caa6b465d8de4ff513d9
Angie Lal <angielal@gmail.com>
2017-04-30 22:20:54 -0700

    Add mocks for Event View Controller.

--- Recreo/Event.swift
+++ Recreo/Event.swift
@@ -20,13 +20,17 @@ class Event: NSObject {
     var venueCoords: CLLocationCoordinate2D?
     var created: Date
     var eventDate: Date?
+    var startTime: Date
+    var endTime: Date?
     var galleryId: Gallery?
+    var eventImage: URL?
     
-    init(eventId: Int, eventName: String, host: User, created: Date) {
+    init(eventId: Int, eventName: String, host: User, created: Date, startTime: Date) {
         self.eventId = eventId
         self.eventName = eventName
         self.host = host
         self.created = created
+        self.startTime = startTime
     }
     
 }
